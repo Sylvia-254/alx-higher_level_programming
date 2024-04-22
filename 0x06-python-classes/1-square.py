ce65faeb2c90de07c99f4068b6910639a7fe286e
Sylvia-254 <cheptoosylvia03@gmail.com>
2024-04-22 14:31:35 +0000

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-# 0-square.py Sylvia Kirui 
+# 0-square.py Sylvia Cheptoo Kirui
 """Defines a square """
 
 class Square:
-"""Represents a square"""
+    """Represents a square"""
 
-def __init__(self, size):
-"""Initializing this square class
-Args: size - represnets the size of the square defined
-"""
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
 
-self.__size = size
+        self.__size = sizei
