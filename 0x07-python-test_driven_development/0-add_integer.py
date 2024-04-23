8b174f82673e5a5fb8da369b6d602d22659dcd6a
Sylvia-254 <cheptoosylvia03@gmail.com>
2024-04-23 09:16:51 +0000

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
-'''
+"""Defines a function add_integer(a, b=98) that adds two integers.
 
-This module has one function that adds up 2 integers
-
-'''
+Attributes:
+    add_integer: function that adds two integers.
+"""
 
 
 def add_integer(a, b=98):
-    '''Return the sum of two integers or floats as integers
+    """Adds two integer and/or float values.
 
     Args:
-        a: first argument
-        b: second argument
-
-    Returns:
-        Sum of the two arguments
+        a (int): First value
+        b (int, optional): Second value. Defaults to 98.
 
     Raises:
-        TypeError: If either of the arguments not an integer or a float
-    '''
-    if ((not isinstance(a, int) and not isinstance(a, float))):
+        TypeError: If a and b are not integers or floats.
+
+    Returns:
+        int: Sum of a and b.
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+
+    if isinstance(a, float):
+        a = int(a)
+
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
