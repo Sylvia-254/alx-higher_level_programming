e1eccc931a9baf7a0fb39cd43d9af7f5bb5defbd
Sylvia-254 <cheptoosylvia@gmail.com>
2024-08-13 06:53:47 +0000

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,55 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+    return;
+  }
+
+  try {
+    const movie = JSON.parse(body);
+    const charactersUrls = movie.characters;
+    const charactersPromises = [];
+
+    charactersUrls.forEach(characterUrl => {
+      charactersPromises.push(new Promise((resolve, reject) => {
+        request(characterUrl, (charError, charResponse, charBody) => {
+          if (charError) {
+            reject(charError);
+            return;
+          }
+
+          if (charResponse.statusCode !== 200) {
+            reject(new Error(`Status: ${charResponse.statusCode}`));
+            return;
+          }
+
+          const character = JSON.parse(charBody);
+          resolve(character.name);
+        });
+      }));
+    });
+
+    Promise.all(charactersPromises)
+      .then(characters => {
+        characters.forEach(characterName => {
+          console.log(characterName);
+        });
+      })
+      .catch(error => {
+        console.error('Error:', error);
       });
-    }
+  } catch (parseError) {
+    console.error('Parse Error:', parseError);
   }
 });
