ba95a840b6a51e51b7cbfea687db001b17066436
Sylvia-254 <cheptoosylvia@gmail.com>
2024-08-13 06:37:42 +0000

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,29 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-const apiUrl = process.argv[2];
-const characterId = '18';
-
-request.get(apiUrl, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
-    return;
-  }
-  if (response.statusCode !== 200) {
-    console.error(`Error: ${response.statusCode}`);
-    return;
-  }
-  try {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    films.forEach(film => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        count++;
-      }
-    });
-    console.log(count);
-  } catch (error) {
-    console.error('Error:', error.message);
+    console.log(error);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
   }
 });
